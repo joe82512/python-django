72bd2ad5a93531e7565da8d47ac933b645b48bd6
joe <joe82512@gmail.com>
2021-12-08 22:58:58 +0800

    docker

---
+++ django_demo/demosite_docker2/demo2/admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Location, Travel
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Location._meta.fields] #全顯示
+    list_per_page = 10 #列表一頁顯示幾項
+    search_fields = ('city',) #搜尋設定 結尾要加逗號組成list/tuple
+    list_filter = ('country',) #過濾器    
+    ordering = ('pk',) #自定義排序
+    # ordering = ('-pk',) #排序, 負號代表反向排列
+
+@admin.register(Travel)
+class TravelAdmin(admin.ModelAdmin):
+    list_display = ['pk','target', 'companion', 'location', 'fk_city', 'date']
+    list_editable = ('location',) #可在列表修改
+    raw_id_fields = ('location',) #外鍵查看, 配合list_editable
+    date_hierarchy = ('date') #根據日期格式分類, 耗時, 不用逗號
+    
+    # 顯示外鍵內容
+    def fk_city(self, obj):
+        return obj.location.city
+    fk_city.admin_order_field  = 'location' #允許排列
+    fk_city.short_description = 'City (fk)' #清單名稱
+
+
+from .models import VerifyCode
+@admin.register(VerifyCode)
+class VerifyCodeAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in VerifyCode._meta.fields] #全顯示
+
+
+### advance ###
+from .models import Food, Drink, Restaurant, City
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Food._meta.fields] #全顯示
+@admin.register(Drink)
+class DrinkAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Drink._meta.fields] #全顯示
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Restaurant._meta.fields] #全顯示
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in City._meta.fields] #全顯示
